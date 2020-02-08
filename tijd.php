e7c11c63358ad88ff20fb836a25b6af10405cdec
Candace <2100978@talnet.nl>
2020-02-08 12:48:17 +0100

    Uitwerking van de exercise

--- tijd.php
+++ tijd.php
@@ -2,15 +2,31 @@
 
 
 
+// echo $input;
 
-echo $input;
-$test = readline("");
-$find = "s";
-$pos = strpos($test, $find);
-
-
-if ($pos === false) {
-    echo "geen tijd gevonden";
-} else {
-    echo "$test seconden";
+foreach ($argv as $arg) {
 }
+
+$pos = strpos($arg, "s");
+
+
+if (strpos($arg, "s") === false) {
+    echo "no Time found";
+} else {
+    echo (int) $arg . " Seconds";
+}
+
+// if ($pos > false) {
+//     echo "geen tijd gevonden";
+// } else {
+//     echo (int) $u . " seconden";
+// }
+
+// $pos = strpos($argv[1], "s");
+
+
+// if ($pos === false) {
+//     echo  "geen tijd gevonden";
+// } else {
+//     echo (int) $argv[1] . " seconden";
+// }
